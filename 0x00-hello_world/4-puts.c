8393279b565ffefccba6299afb10d760f5f28d5d
HitimanaEd <edhitimana15@gmail.com>
2022-12-12 12:00:48 -0800

    Task 4 done!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,17 @@
 
 /**
  *
- *  * main - Entry
+ *  * main - Entry Point
  *
- *   * Return: Always 0
+ *   * Return: Always 0 (Success)
  *
  *    */
 
 int main(void)
 {                    
 
-	puts ( "\"Programming is like building a multilingual puzzle" );
-	return ( 0 );
+	puts ( "\"Programming is like building a multilingual puzzle");
+	return (0);
 
 
 
